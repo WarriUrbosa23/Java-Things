cae794245172fa181bb4c6e123d9ee6a6bd49e9b
Mihir Pillay <thekiddojac@gmail.com>
2024-12-03 20:02:55 -0800

    Small mistake

--- palindromes2.java
+++ palindromes2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class palindromes{
+public class palindromes2{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("String: ");
